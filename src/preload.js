cd863cb585f72892698f6a71abb9f90bd2f7f485
flitz123 <127119426+flitz123@users.noreply.github.com>
2025-08-28 11:58:53 +0300

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -11,11 +11,23 @@ contextBridge.exposeInMainWorld('electronAPI', {
   // App management
   showAppPicker: () => ipcRenderer.invoke('show-app-picker'),
   getInstalledApps: () => ipcRenderer.invoke('get-installed-apps'),
+  launchApp: (appName) => ipcRenderer.invoke('launch-app', appName),
   
   // Block list management
   getBlockList: () => ipcRenderer.invoke('get-block-list'),
   addToBlockList: (appName) => ipcRenderer.invoke('add-to-block-list', appName),
   removeFromBlockList: (appName) => ipcRenderer.invoke('remove-from-block-list', appName),
+  
+  // Window management
+  minimizeWindow: () => ipcRenderer.invoke('minimize-window'),
+  
+  // Event listeners
+  onAppBlocked: (callback) => ipcRenderer.on('app-blocked', callback),
+  onSessionStarted: (callback) => ipcRenderer.on('session-started', callback),
+  onSessionEnded: (callback) => ipcRenderer.on('session-ended', callback),
+  
+  // Cleanup
+  removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel),
   
   // System info
   platform: process.platform
